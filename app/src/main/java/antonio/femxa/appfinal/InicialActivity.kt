9c66c0c6682ce549faeb515630dd34a2aa72ea5b
valexx55 <valexx55@gmail.com>
2025-10-22 13:41:33 +0200

    subida con TODOS

--- app/src/main/java/antonio/femxa/appfinal/InicialActivity.kt
+++ app/src/main/java/antonio/femxa/appfinal/InicialActivity.kt
@@ -15,6 +15,24 @@ class InicialActivity : AppCompatActivity() {
      var mediaPlayer: MediaPlayer? = null
      var musicaOnOff: Boolean = false
 
+    /**
+     * TODO SPLASHSCREEN
+     * TODO CATEGORIAS - EXTERNALIZAR EN UN API GITHUB / AMPLIAR CATEGORÍAS Y PALABRAS
+     * TODO PROBLEMA TECLADO DIMENSIÓN DE LAS LETRAS / TIPO DE FUENTE / TAMAÑO DE LAS TECLAS
+     * TODO PALALBRA A RESOLVER NO SE VE EN EMULADOR - problema color tema oscuro
+     * TODO PROBLEMA SONIDO INCONSISTENT
+     * TODO CRÉDITOS REFORMULAR cambiar huevo de pascua INCLUIR A aitor.garcia.ruiz@hotmail.com NAVALCARNERO
+     * TODO revisar tHENNAS Y TEMA
+     * TODO AÑADIR ALGUNA ANIMACIÓN
+     * TODO AÑADIR ANUNCIOS
+     * TODO REVISAR NAVEGACIÓN
+     * TODO CATEGORIAS TAMAÑO SPINNER PARA QUE SE VEA BIEN TODO
+     * TODO CAPITALIZACIÓN DERROTA VICTORIA ACTIVITY
+     * TODO REVISAR MEJORA APP ICONO VISIBLIDAD
+     * TODO eliminar referencias a femxa
+     */
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicial)
